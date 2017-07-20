117e87d072b69125478860c8b922b54102ffc145
Ken Gardiner <ken_gardiner@yahoo.com>
2017-07-20 06:14:48 -0400

    Had to finish StudentQuizScores.  Had to.

--- StudentQuizScores/src/main/java/com/mycompany/studentquizscores/StudentQuizScoresApp.java
+++ StudentQuizScores/src/main/java/com/mycompany/studentquizscores/StudentQuizScoresApp.java
@@ -17,21 +17,27 @@ public class StudentQuizScoresApp {
 
     public static void main(String[] args) {
 
+        //initialize variables
         UserIO appIO = new UserIOImpl();
         HashMap<String, ArrayList<Integer>> quizGrades = new HashMap<>();
+        String studentName = "";
         int menuChoice = 0;
         ArrayList<Integer> gradesOut = new ArrayList<>();
-        String menu = "1. View a list of students in the system\n"
+        //big String menu
+        String menu = "========================Menu======================= \n"
+                + "1. View a list of students in the system\n"
                 + "2. Add a student to the system\n"
                 + "3. Remove a student from the system\n"
                 + "4. View a list of quiz scores for a given student\n"
                 + "5. View the average quiz score for a given student\n"
                 + "6. Exit System\n"
+                + "=================================================== \n"
                 + "Please enter your selection: ";
 
         //add some test data
         quizGrades.put("Ken", new ArrayList<>(Arrays.asList(70, 80, 90)));
 
+        //Logic, IO, etc
         do {
             menuChoice = appIO.readInt(menu, 1, 6);
 
@@ -54,14 +60,21 @@ public class StudentQuizScoresApp {
                 case 4:
                     appIO.print("Student Grade List: ");
 
-                    for (String student : quizGrades.keySet()) {
-                        for (int i = 0; i < quizGrades.get(student).size(); i++) {
-                            System.out.println(quizGrades.get(student).get(i));
-                        }
+                    studentName = appIO.readString("Student name: ");
+                    for (int i = 0; i < quizGrades.get(studentName).size(); i++) {
+                        System.out.print(quizGrades.get(studentName).get(i) + ", ");
                     }
+                    appIO.print("");
                     break;
                 case 5:
+                    appIO.print("Student Quiz Average: ");
 
+                    studentName = appIO.readString("Student name: ");
+                    double sum = 0.0;
+                    for (int i = 0; i < quizGrades.get(studentName).size(); i++) {
+                        sum += quizGrades.get(studentName).get(i);
+                    }
+                    System.out.println("Quiz Average: " + (sum / quizGrades.get(studentName).size()));
                     break;
                 case 6:
 
