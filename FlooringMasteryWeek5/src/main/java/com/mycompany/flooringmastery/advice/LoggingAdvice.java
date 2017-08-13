bc55e3c0311c3329bd6955923b3fd1b321655d4f
Ken Gardiner <ken_gardiner@yahoo.com>
2017-08-13 16:52:32 -0400

    KENGARDINERINDIVIDUALWORK-16 Logging added.

--- FlooringMasteryWeek5/src/main/java/com/mycompany/flooringmastery/advice/LoggingAdvice.java
+++ FlooringMasteryWeek5/src/main/java/com/mycompany/flooringmastery/advice/LoggingAdvice.java
@@ -8,6 +8,7 @@ package com.mycompany.flooringmastery.advice;
 import com.mycompany.flooringmastery.dao.FlooringMasteryAuditDao;
 import com.mycompany.flooringmastery.dao.FlooringMasteryPersistenceException;
 import com.mycompany.flooringmastery.service.ItemNotAvailableException;
+import com.mycompany.flooringmastery.service.TaxException;
 import org.aspectj.lang.JoinPoint;
 
 /**
@@ -22,62 +23,62 @@ public class LoggingAdvice {
         this.auditDao = auditDao;
     }
 
-//    //Exception catches all exceptions
-//    public void createAuditEntryIFE(JoinPoint jp, InsufficientFundsException ex) {
-//
-//        Object[] args = jp.getArgs();
-//        //getsignature.getname = need of the calling method "getStudent" "removeStudent"
-//        String auditEntry = "Method name: " + jp.getSignature().getName() + " | ";
-//
-//        auditEntry += "Item Requested: ";
-//        for (Object currentArg : args) {
-//            auditEntry += currentArg;
-//        }
-//        auditEntry += " | InsufficientFundsException: " + ex;
-//        try {
-//            auditDao.writeAuditEntry(auditEntry);
-//        } catch (FlooringMasteryPersistenceException e) {
-//            System.err.println(
-//                    "ERROR: Could not create audit entry in LoggingAdvice.");
-//        }
-//    }
-//
-//    public void createAuditEntryNII(JoinPoint jp, ItemNotAvailableException ex) {
-//        Object[] args = jp.getArgs();
-//        //getsignature.getname = need of the calling method "getStudent" "removeStudent"
-//        String auditEntry = "Method name: " + jp.getSignature().getName() + " | ";
-//
-//        auditEntry += "Item Requested: ";
-//        for (Object currentArg : args) {
-//            auditEntry += currentArg;
-//        }
-//        auditEntry += " | ItemNotAvailableException: " + ex;
-//        try {
-//            auditDao.writeAuditEntry(auditEntry);
-//        } catch (FlooringMasteryPersistenceException e) {
-//            System.err.println(
-//                    "ERROR: Could not create audit entry in LoggingAdvice.");
-//        }
-//    }
-//
-//    public void createAuditEntry(JoinPoint jp) {
-//        //JoinPoint - is the object that carries the information about the joinpoint or pointcut (the place where 
-//        //this will be used.  Let's us create this method in a generic way.
-//
-//        //paramters can be of any type, so we need an array of objects
-//        Object[] args = jp.getArgs();
-//        //getsignature.getname = need of the calling method "getStudent" "removeStudent"
-//        String auditEntry = "Method name: " + jp.getSignature().getName() + " | ";
-//        auditEntry += "Item successfully purchased: ";
-//        for (Object currentArg : args) {
-//            auditEntry += currentArg;
-//        }
-//        try {
-//            auditDao.writeAuditEntry(auditEntry);
-//        } catch (FlooringMasteryPersistenceException e) {
-//            System.err.println(
-//                    "ERROR: Could not create audit entry in LoggingAdvice.");
-//        }
-//    }
+    //Exception catches all exceptions
+    public void createAuditEntryINAE(JoinPoint jp, ItemNotAvailableException ex) {
+
+        Object[] args = jp.getArgs();
+        //getsignature.getname = need of the calling method "getStudent" "removeStudent"
+        String auditEntry = "Method name: " + jp.getSignature().getName() + " | ";
+
+        auditEntry += "Item Requested: ";
+        for (Object currentArg : args) {
+            auditEntry += currentArg;
+        }
+        auditEntry += " | ItemNotAvailableException: " + ex;
+        try {
+            auditDao.writeAuditEntry(auditEntry);
+        } catch (FlooringMasteryPersistenceException e) {
+            System.err.println(
+                    "ERROR: Could not create audit entry in LoggingAdvice.");
+        }
+    }
+
+    public void createAuditEntryTE(JoinPoint jp, TaxException ex) {
+        Object[] args = jp.getArgs();
+        //getsignature.getname = need of the calling method "getStudent" "removeStudent"
+        String auditEntry = "Method name: " + jp.getSignature().getName() + " | ";
+
+        auditEntry += "Item Requested: ";
+        for (Object currentArg : args) {
+            auditEntry += currentArg;
+        }
+        auditEntry += " | Tax Exception: " + ex;
+        try {
+            auditDao.writeAuditEntry(auditEntry);
+        } catch (FlooringMasteryPersistenceException e) {
+            System.err.println(
+                    "ERROR: Could not create audit entry in LoggingAdvice.");
+        }
+    }
+
+    public void createAuditEntry(JoinPoint jp) {
+        //JoinPoint - is the object that carries the information about the joinpoint or pointcut (the place where 
+        //this will be used.  Let's us create this method in a generic way.
+
+        //paramters can be of any type, so we need an array of objects
+        Object[] args = jp.getArgs();
+        //getsignature.getname = need of the calling method "getStudent" "removeStudent"
+        String auditEntry = "Method name: " + jp.getSignature().getName() + " | ";
+        auditEntry += "Order successfully added: ";
+        for (Object currentArg : args) {
+            auditEntry += currentArg;
+        }
+        try {
+            auditDao.writeAuditEntry(auditEntry);
+        } catch (FlooringMasteryPersistenceException e) {
+            System.err.println(
+                    "ERROR: Could not create audit entry in LoggingAdvice.");
+        }
+    }
 
 }
